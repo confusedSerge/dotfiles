61eeca3f94a016511d40dcd7f9ada742c13df783
kotserge <serge.kotchourko@gmail.com>
2026-03-05 19:02:00 +0100

    ref: adjusted config

--- config/waybar/launch.sh
+++ config/waybar/launch.sh
@@ -5,7 +5,7 @@ dir="$HOME/.config/waybar"
 launch_bar() {
     killall waybar
     while pgrep waybar > /dev/null; do killall waybar; done
-    waybar -c "$dir/config.jsonc" -s "$dir/pill.css" &
+    waybar -c "$dir/config.jsonc" -s "$dir/rounded.css" &
 }
 
 launch_bar
